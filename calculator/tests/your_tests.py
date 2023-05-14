2cef07d8e454811a8c27d61669cff19c1c87a902
michaelsouliman <michaelsouliman@gmail.com>
2023-05-14 16:37:18 -0700

    Added Makefile and custom tests

--- calculator/tests/your_tests.py
+++ calculator/tests/your_tests.py
@@ -3,7 +3,10 @@ from calculator_adapter import run
 
 
 ### ADD AT LEAST TWO TESTS HERE!
-
+#Check that division output is correct
+assert run("15 / 3").output == "5"
+#Check that subtraction output is correct
+assert run("15 - 10").output == "5"
 
 ###
 
